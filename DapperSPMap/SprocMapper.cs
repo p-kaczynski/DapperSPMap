b80518d1fb6dd97dcc932d5b007b35edda9c0d51
Pawel Kaczynski <pawel.kaczynski@digirati.co.uk>
2015-05-22 13:30:22 +0100

    Added xml-doc

--- DapperSPMap/SprocMapper.cs
+++ DapperSPMap/SprocMapper.cs
@@ -9,6 +9,12 @@ namespace DapperSPMap
         private static readonly Dictionary<Type, Dictionary<string, object>> Configuration =
             new Dictionary<Type, Dictionary<string, object>>();
 
+        /// <summary>
+        /// Creates a new named map for {TModel} that describres how to obtain <see cref="DynamicParameters"/> from it
+        /// </summary>
+        /// <typeparam name="TModel">any type to be mapped</typeparam>
+        /// <param name="mapName">type-unique identifier of this map</param>
+        /// <returns></returns>
         public static ISprocMappingExpression<TModel> CreateMap<TModel>(string mapName)
         {
             var expr = new SprocMappingExpression<TModel>();
@@ -21,6 +27,13 @@ namespace DapperSPMap
             return expr;
         }
 
+        /// <summary>
+        /// Obtain single <see cref="DynamicParameters"/> object from provided {TModel} entity. The mapping used must be of single type.
+        /// </summary>
+        /// <typeparam name="TModel">item's type</typeparam>
+        /// <param name="item">item to map into <see cref="DynamicParameters"/></param>
+        /// <param name="mapName">mapping name used with <see cref="SprocMapper.CreateMap{TModel}"/> method</param>
+        /// <returns>a single DynamicParameters object with parameters reflecting mapped item</returns>
         public static DynamicParameters GetParameters<TModel>(TModel item, string mapName)
         {
             var expr = GetExpression<TModel>(mapName);
@@ -28,6 +41,13 @@ namespace DapperSPMap
             return expr.GetParameters(item);
         }
 
+        /// <summary>
+        /// Obtain a collection of <see cref="DynamicParameters"/> objects from provided {TModel} IEnumerable. The mapping used must be of multi type.
+        /// </summary>
+        /// <typeparam name="TModel">items type</typeparam>
+        /// <param name="items">items to map to <see cref="DynamicParameters"/></param>
+        /// <param name="mapName">mapping name used with <see cref="SprocMapper.CreateMap{TModel}"/> method</param>
+        /// <returns>Grouped and/or aggregated items data, mapped into <see cref="DynamicParameters"/> objects</returns>
         public static IEnumerable<DynamicParameters> GetParameterSets<TModel>(IEnumerable<TModel> items, string mapName)
         {
             var expr = GetExpression<TModel>(mapName);
@@ -35,6 +55,12 @@ namespace DapperSPMap
             return expr.GetParameterSets(items);
         }
 
+        /// <summary>
+        /// Retrieves named mapping expression for {TModel}, or throws if anything is wrong
+        /// </summary>
+        /// <typeparam name="TModel">type for which map is retrieved</typeparam>
+        /// <param name="mapName">mapping name used with <see cref="SprocMapper.CreateMap{TModel}"/> method</param>
+        /// <returns></returns>
         private static ISprocMappingExpression<TModel> GetExpression<TModel>(string mapName)
         {
             if (!Configuration.ContainsKey(typeof (TModel)))
@@ -57,6 +83,11 @@ namespace DapperSPMap
             return expr;
         }
 
+        /// <summary>
+        /// Remove specified map for {TModel} type - if exists.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="mapName"></param>
         public static void RemoveMap<TModel>(string mapName)
         {
             if (!Configuration.ContainsKey(typeof (TModel))) return;
@@ -66,6 +97,10 @@ namespace DapperSPMap
             Configuration[typeof (TModel)].Remove(mapName);
         }
 
+        /// <summary>
+        /// Remove all models for specified {TModel}, if any
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
         public static void RemoveAllMaps<TModel>()
         {
             if (!Configuration.ContainsKey(typeof(TModel))) return;
